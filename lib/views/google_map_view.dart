0d08358c1de9480342e7e02b67c78a0c8109d3df
AswiniKrishna05 <aswinikrishna05@gmail.com>
2025-07-24 23:10:30 +0530

    map

--- lib/views/google_map_view.dart
+++ lib/views/google_map_view.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
